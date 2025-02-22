5d7b995478b82767d2a15075829a192d5591f8af
unknown <ivanafariash@gmail.com>
2025-02-22 18:01:50 -0400

    custom permissions

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,16 @@
 from rest_framework.permissions import permissions
 from rest_framework import generics
 from .models import Post
+from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
-class PostList(generic.views.RetrieveUpdateDestroyAPIView)
+class PostList(generic.ListCreateAPIView)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permission.IsAdminUser)
+    permission_classes = (IsAuthorOrReadOnly)
+
 
 class PostDetail(generic.views.RetrieveUpdateDestroyAPIView)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly)
